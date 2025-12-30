e21e10c4bc696f4e4033963b8fcf5a93719d3320
Tyler Onstad-Cran <tonstadcran@gmail.com>
2025-12-30 12:05:05 -0800

    lexer rewrite

--- src/lexing/lexer.rs
+++ src/lexing/lexer.rs
@@ -1,84 +1,134 @@
 use crate::lexing::token::Token;
 use regex::Regex;
 
-pub fn lex_program(program: &str) -> Vec<Token> {
-    let current_input = program;
+// //TODO: can't be global matching has to be anchored to index
+// pub fn lex_program(program: &str) -> Vec<Token> {
+//     let current_input = program;
 
-    //highest to lowest priority
-    //ADD NEW TOKENS
-    let tokens = [
-        "Print",
-        "If",
-        "Else",
-        "While",
-        "For",
-        "Fun",
-        "Class",
-        "Super",
-        "Return",
-        "This",
-        "And",
-        "Or",
-        "Null",
-        "Plus",
-        "Minus",
-        "Star",
-        "Slash",
-        "Dot",
-        "Assign",
-        "Semicolon",
-        "LeftParen",
-        "RightParen",
-        "LeftBrace",
-        "RightBrace",
-        "Bang",
-        "BangEqual",
-        "GreaterEqual",
-        "LessEqual",
-        "EqualEqual",
-        "GreaterThan",
-        "LessThan",
-        "IntegerLiteral",
-        "StringLiteral",
-        "True",
-        "False",
-        "Identifier",
-    ];
+//     //highest to lowest priority
+//     //ADD NEW TOKENS
+//     let tokens = [
+//         "Print",
+//         "If",
+//         "Else",
+//         "While",
+//         "For",
+//         "Fun",
+//         "Class",
+//         "Super",
+//         "Return",
+//         "This",
+//         "And",
+//         "Or",
+//         "Null",
+//         "Plus",
+//         "Minus",
+//         "Star",
+//         "Slash",
+//         "Dot",
+//         "Assign",
+//         "Semicolon",
+//         "LeftParen",
+//         "RightParen",
+//         "LeftBrace",
+//         "RightBrace",
+//         "Bang",
+//         "BangEqual",
+//         "GreaterEqual",
+//         "LessEqual",
+//         "EqualEqual",
+//         "GreaterThan",
+//         "LessThan",
+//         "IntegerLiteral",
+//         "StringLiteral",
+//         "True",
+//         "False",
+//         "Identifier",
+//     ];
 
-    let mut match_vec: Vec<(&str, usize, usize)> = Vec::new();
+//     let mut match_vec: Vec<(&str, usize, usize)> = Vec::new();
 
-    for token in tokens.iter() {
-        let token_regex = Token::get_token_regex(token);
-        match token_regex {
-            Ok(t) => {
-                let re = Regex::new(t.as_str()).unwrap();
-                let matched = re.find_iter(current_input);
+//     for token in tokens.iter() {
+//         let token_regex = Token::get_token_regex(token);
+//         match token_regex {
+//             Ok(t) => {
+//                 let re = Regex::new(t.as_str()).unwrap();
+//                 let matched = re.find_iter(current_input);
 
-                let all_matches = matched.collect::<Vec<_>>();
+//                 let all_matches = matched.collect::<Vec<_>>();
 
-                if all_matches.len() == 0 {
-                    continue;
-                }
+//                 if all_matches.len() == 0 {
+//                     continue;
+//                 }
 
-                for m in all_matches.iter() {
-                    match_vec.push((token, m.start(), m.end()));
-                }
-            }
-            Err(e) => eprintln!("{}", e),
-        };
-    }
+//                 for m in all_matches.iter() {
+//                     match_vec.push((token, m.start(), m.end()));
+//                 }
+//             }
+//             Err(e) => eprintln!("{}", e),
+//         };
+//     }
 
-    match_vec.sort_by(|a, b| a.1.cmp(&b.1).then_with(|| (b.2 - b.1).cmp(&(a.2 - a.1))));
+//     match_vec.sort_by(|a, b| a.1.cmp(&b.1).then_with(|| (b.2 - b.1).cmp(&(a.2 - a.1))));
 
-    let mut token_vec: Vec<Token> = Vec::new();
-    for m in match_vec.iter() {
-        let token = Token::get_token(m.0, Some(&current_input[m.1..m.2]));
-        match token {
-            Ok(t) => token_vec.push(t),
-            Err(e) => eprintln!("{}", e),
+//     let mut token_vec: Vec<Token> = Vec::new();
+//     for m in match_vec.iter() {
+//         let token = Token::get_token(m.0, Some(&current_input[m.1..m.2]));
+//         match token {
+//             Ok(t) => token_vec.push(t),
+//             Err(e) => eprintln!("{}", e),
+//         }
+//         //token_vec.push(Token::get_token(m.0, Some(&current_input[m.1..m.2])));
+//     }
+
+//     return token_vec;
+// }
+
+struct Lexer<'a> {
+    source: &'a str,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Lexer {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
         }
-        //token_vec.push(Token::get_token(m.0, Some(&current_input[m.1..m.2])));
     }
 
-    return token_vec;
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn advance(&mut self) -> char {
+        let c = self.source.as_bytes()[self.current] as char;
+        self.current += 1;
+        c
+    }
+
+    fn add_token(&mut self, token: Token) {
+        self.tokens.push(token);
+    }
+
+    fn scan_token(&mut self) {
+        let c = self.advance();
+    }
+}
+
+pub fn lex_program(source: &str) -> Vec<Token> {
+    let mut lexer = Lexer::new(source);
+
+    while !lexer.is_at_end() {
+        lexer.start = lexer.current;
+        lexer.scan_token();
+    }
+
+    lexer.tokens
 }
